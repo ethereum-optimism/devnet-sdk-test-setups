a067c0b292c91c8e92874f3b8d905a496903e680
Jan Nanista <jan.jakub.nanista@gmail.com>
2025-02-25 15:04:01 -0800

    chore: Comments in the smoke test

--- kurtosis/smoke_test.go
+++ kurtosis/smoke_test.go
@@ -10,53 +10,64 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// TestSystemBuildsBlocks ensures that all L2 chains build 100 blocks within specified timeframe
+//
+// FIXME https://github.com/ethereum-optimism/platforms-team/issues/650 is required to access all RPC URLs in a system
 func TestSystemBuildsBlocks(t *testing.T) {
 	lowLevelSysGetter, lowLevelSysValidator := validators.AcquireLowLevelSystem()
 
-	t.Run("system keeps building blocks", func(t *testing.T) {
-		systest.SystemTest(t,
-			func(t systest.T, sys system.System) {
-				ctx := t.Context()
+	systest.SystemTest(t,
+		func(t systest.T, sys system.System) {
+			ctx := t.Context()
 
-				lowLevelSys := lowLevelSysGetter(ctx)
-				l2s := lowLevelSys.L2s()
+			lowLevelSys := lowLevelSysGetter(ctx)
+			l2s := lowLevelSys.L2s()
 
-				numAttempts := 100
-				targetBlockNumber := uint64(100)
-				for attempt := range 100 {
-					t.Logf("Checking blocks, attempt %d/%d", attempt, numAttempts)
+			numAttempts := 100
+			targetBlockNumber := uint64(100)
+			for attempt := range 100 {
+				t.Logf("Checking blocks, attempt %d/%d", attempt, numAttempts)
 
-					pendingL2s := []system.LowLevelChain{}
+				// We'll accumulate all L2s that have not yet reached the block target in this list
+				pendingL2s := []system.LowLevelChain{}
 
-					for _, l2 := range l2s {
-						client, err := l2.Client()
-						require.NoError(t, err)
+				for _, l2 := range l2s {
+					// We get hold of a client
+					client, err := l2.Client()
+					require.NoError(t, err)
 
-						blockNumber, err := client.BlockNumber(ctx)
-						require.NoError(t, err)
+					// We ask the node for its block number
+					blockNumber, err := client.BlockNumber(ctx)
+					require.NoError(t, err)
 
-						t.Logf("Chain %d: Got block %d", l2.ID(), blockNumber)
+					if blockNumber < targetBlockNumber {
+						// If the chain has not yet reached the block target, we push it into the pending list
+						t.Logf("Chain %d: Pending, block %d", l2.ID(), blockNumber)
 
-						if blockNumber < targetBlockNumber {
-							pendingL2s = append(pendingL2s, l2)
-						}
-
+						pendingL2s = append(pendingL2s, l2)
+					} else {
+						// If the chain has reached the target we log and move on
 						t.Logf("Chain %d: Reached block target", l2.ID())
 					}
 
-					l2s = pendingL2s
-					if len(l2s) == 0 {
-						t.Logf("Reached block targets for all chains")
-						return
-					}
-
-					time.Sleep(5 * time.Second)
 				}
 
-				require.FailNow(t, "Did not reach target block number in time")
+				// We update the list of pending chains
+				l2s = pendingL2s
 
-			},
-			lowLevelSysValidator,
-		)
-	})
+				// And if there are no pending chains, we happily exit
+				if len(l2s) == 0 {
+					t.Logf("Reached block targets for all chains")
+					return
+				}
+
+				// If there are any pending chains left, we sleep and poll again
+				time.Sleep(5 * time.Second)
+			}
+
+			require.FailNow(t, "Did not reach target block number in time")
+
+		},
+		lowLevelSysValidator,
+	)
 }
