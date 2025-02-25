4cb46ffe9d4dd284ac1c7b241badf4bb473d3563
Jan Nanista <jan.jakub.nanista@gmail.com>
2025-02-25 15:20:20 -0800

    fix: Wallet test

--- kurtosis/wallet_test.go
+++ kurtosis/wallet_test.go
@@ -12,6 +12,9 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// TestAcquireWalletWithFundsExample is an example test that uses AcquireL2WalletWithFunds validator
+//
+// This is useful when a funded L2 account is needed to run the test
 func TestAcquireWalletWithFundsExample(t *testing.T) {
 	chainIdx := uint64(0)
 	requiredBalance := sdktypes.NewBalance(big.NewInt(1.0 * constants.ETH))
@@ -22,7 +25,7 @@ func TestAcquireWalletWithFundsExample(t *testing.T) {
 			ctx := t.Context()
 
 			wallet := walletGetter(ctx)
-			require.Greater(t, wallet.Balance(), sdktypes.NewBalance(big.NewInt(0)))
+			require.Greater(t, wallet.Balance().Int, sdktypes.NewBalance(big.NewInt(0)).Int)
 		},
 		walletValidator,
 	)
